dafe7a85ef3d3f1d634555f5ccce431716708210
Michelle Sri <michelle.sri@gmail.com>
2016-06-23 17:52:40 -0700

    switched to michelles computer and michelle is driving now

--- scripts/articleController.js
+++ scripts/articleController.js
@@ -7,7 +7,7 @@
     articleView.index(ctx.articles);
   };
 
-  // COMMENT: What does this method do?  What is it's execution path?
+  // COMMENT (done): What does this method do?  What is it's execution path?
   // It takes in a context object and another function and then it creates a
   //  function that will take an article and attach it to the ctx object with
   //  articles as the key.  Then it calles the function next and passes it the
@@ -23,7 +23,7 @@
     Article.findWhere('id', ctx.params.id, articleData);
   };
 
-  // COMMENT: What does this method do?  What is it's execution path?
+  // COMMENT (done): What does this method do?  What is it's execution path?
   // It is a function that takes a context parameter and another function, Then
   //  declares a variable that contains a function that takes the articlesByAuthor
   //  parameter and stores it in the context object under the articles key. Then
@@ -40,7 +40,7 @@
     Article.findWhere('author', ctx.params.authorName.replace('+', ' '), authorData);
   };
 
-  // COMMENT: What does this method do?  What is it's execution path?
+  // COMMENT(done): What does this method do?  What is it's execution path?
   // Same as above, only with the category as the search parameter.
   articlesController.loadByCategory = function(ctx, next) {
     var categoryData = function(articlesInCategory) {
@@ -51,7 +51,7 @@
     Article.findWhere('category', ctx.params.categoryName, categoryData);
   };
 
-  // COMMENT: What does this method do?  What is it's execution path?
+  // COMMENT (done): What does this method do?  What is it's execution path?
   // Again, we create a closure, but this time it will assign Article.all to the
   //  context object then call the next function which will get the ctx object
   //  as a parameter.
